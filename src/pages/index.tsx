4fbb5f2bb16839286c1eb74d314f54a8c158398e
Pierre Gabriel <pierre.alexander.mg@gmail.com>
2025-05-20 20:45:12 -0300

    removed unnecessary state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react';
 import { OrderForm } from '../components/OrderForm';
 import { OrderList } from '../components/OrderList/OrderList';
 
 export default function HomePage() {
-  const [reload, setReload] = useState(false);
 
   return (
     <div className="p-[30px]">
       <h1 className="text-[30px] font-bold mb-[30px]">Sistema de Ordens</h1>
-      <OrderForm onOrderCreated={() => setReload(!reload)} />
-      <OrderList key={reload ? 'a' : 'b'} />
+      <OrderForm />
+      <OrderList/>
     </div>
   );
 }
